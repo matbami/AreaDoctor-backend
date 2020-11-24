ea33c280c94f8a32e0f47274df1221f65465e935
korlahwarleh <jmkolawole@gmail.com>
2020-11-24 14:03:56 +0100

    Authentication from Kola

---
+++ models/users.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passortLocalMongoose = require('passport-local-mongoose');
+
+
+var User = new Schema({
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
+    facebookId : String, 
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+
+User.plugin(passortLocalMongoose);
+module.exports = mongoose.model('User', User);
